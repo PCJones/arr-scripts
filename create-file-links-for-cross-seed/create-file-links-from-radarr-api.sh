404b37f722869247ba569d606cbcb476159b9259
Jonas F <github@pcjones.de>
2024-04-10 02:17:19 +0200

    Update create-file-links-from-radarr-api.sh

--- create-file-links-for-cross-seed/create-file-links-from-radarr-api.sh
+++ create-file-links-for-cross-seed/create-file-links-from-radarr-api.sh
@@ -66,7 +66,6 @@ fi
 page=1
 totalPages=1
 
-# Function to process files
 process_files() {
     local sourceTitle="$1"
     local droppedPath="$2"
@@ -76,29 +75,22 @@ process_files() {
     droppedPath=$(apply_path_mappings "$droppedPath")
     importedPath=$(apply_path_mappings "$importedPath")
 
-    # Extract base file name without extension for importedPath, and escape it
     local baseFileNameImportedEscaped=$(printf '%q' "$(basename "$importedPath" | rev | cut -d. -f2- | rev)")
-    # Extract base file name without extension for droppedPath, and escape it
     local baseFileNameDroppedEscaped=$(printf '%q' "$(basename "$droppedPath" | rev | cut -d. -f2- | rev)")
-
-    # Log the directory about to be searched
     local searchDirectory="$(dirname "$importedPath")"
     log "Searching directory: $searchDirectory for files matching base name: $baseFileNameImportedEscaped.*"
 
-    # Check if directory exists before attempting to find to avoid the error
     if [ ! -d "$searchDirectory" ]; then
         log "Error: Search directory does not exist: $searchDirectory"
-        return # Exit the function to avoid further errors
+        return
     fi
 
-    # Temporarily store the matching files in an array
     local matchingFiles=($(find "$searchDirectory" -type f -name "${baseFileNameImportedEscaped}.*"))
     if [ ${#matchingFiles[@]} -eq 0 ]; then
         log "No matching files found for: $baseFileNameImportedEscaped.*"
-        return # Exit the function as there are no files to process
+        return
     fi
 
-    # Create the destination folder if there are files to link and if not in dry run
     if [[ "$DRY_RUN" == "false" ]]; then
         mkdir -p "$DESTINATION_FOLDER/$sourceTitle" && log "Created destination folder: $DESTINATION_FOLDER/$sourceTitle"
     else
@@ -113,7 +105,6 @@ process_files() {
 
         log "Processing file: $filePath. Target file name: $finalFileName"
 
-        # Create hardlink or symbolic link based on configuration, using the final file name
         if [[ -n "$USE_SYMBOLIC_LINKS" && "$DRY_RUN" == "false" ]]; then
             ln -s "$filePath" "$DESTINATION_FOLDER/$sourceTitle/$finalFileName" && log "Created symbolic link for $filePath at $DESTINATION_FOLDER/$sourceTitle/$finalFileName"
         elif [[ "$DRY_RUN" == "false" ]]; then
@@ -130,16 +121,12 @@ process_files() {
 
 
 log "Starting processing of pages. Initial Page: $page, Total Pages Calculated: $totalPages"
-
-# Loop through all pages
-# Initialize associative array to keep track of processed movie IDs
 declare -A processedMovieIds
 
 while [ $page -le $totalPages ]; do
     log "Fetching data for page $page of $totalPages"
     response=$(curl -s -G "${RADARR_URL}/api/v3/history" --data-urlencode "apikey=${API_KEY}" --data "page=${page}" --data "includeMovie=true" --data "eventType=3")
     
-    # Parse total page count from the first request
     if [[ $page -eq 1 ]]; then
         totalPages=$(echo "$response" | jq '.totalRecords')
         totalPages=$((totalPages + 9 / 10)) # Calculate total pages based on 10 records per page
@@ -155,7 +142,6 @@ while [ $page -le $totalPages ]; do
         if [[ -z "${processedMovieIds[$movieId]}" ]]; then
             log "Processing record for $sourceTitle"
             process_files "$sourceTitle" "$droppedPath" "$importedPath"
-            # Mark movieId as processed
             processedMovieIds[$movieId]=1
         else
             log "Skipping already processed movieId: $movieId for $sourceTitle"
